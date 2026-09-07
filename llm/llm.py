cb59146424603a039a7e6608325cf69051c2c5ef
UzairAtiq <uzairatiq65@gmail.com>
2026-09-07 14:05:46 +0500

    feat: create Groq client, invoke, and return response content

---
+++ llm/llm.py
@@ -0,0 +1,18 @@
+from langchain_groq import ChatGroq
+from llm.prompt import prompt
+from sandbox.runner import runner
+from config import GROQ_API_KEY
+
+def llm() :
+
+  #Setiing up the llm
+  llm = ChatGroq(
+      model="openai/gpt-oss-120b",
+      api_key=GROQ_API_KEY
+  )
+
+  #sending prompt to llm to get fix
+  answer = llm.invoke(prompt)
+
+  #Returning only the text content
+  return answer.content
