f6f8cd95f2c6f7d347c90ce78b31eee69e2872e4
UzairAtiq <uzairatiq65@gmail.com>
2026-09-08 16:40:32 +0500

    feat: separate function code from assertions before sending to LLM, reattach unmodified assertions after each fix

--- agent/loop.py
+++ agent/loop.py
@@ -2,10 +2,31 @@ from agent.fixer import get_fix
 from sandbox.runner import runner
 from agent.stopping import stopper
 
-def fix_loop (test_file : str) :
+
+
+def fix_loop (test_file : str , fixed_file) :
 
   #count variable for counting how mant times the loop has run
   count = 0
+
+  #copying the broken files code into a variable for the fixer
+
+  #Reading the code from the test file 
+  with open(test_file, "r") as f:
+      code = f.read()
+
+  #Splitting the fucntion and assert statements  
+  lines = code.splitlines()
+
+  function_lines = [line for line in lines if not line.strip().startswith("assert")]
+  assert_lines = [line for line in lines if line.strip().startswith("assert")]
+
+  function = "\n".join(function_lines)
+  assert_statements = "\n".join(assert_lines)
+
+  #Writing the code to the fixed file 
+  with open(fixed_file, "w") as f:
+          f.write(function + "\n" + assert_statements)
 
   #Fixer loop
   while True :
@@ -17,7 +38,7 @@ def fix_loop (test_file : str) :
     print("Attempt # : ",count)
 
     #Running the file through the runner(executor)
-    result = runner(test_file)
+    result = runner(fixed_file)
 
     #If no error
     if result.returncode == 0 or stopper(count):
@@ -33,14 +54,14 @@ def fix_loop (test_file : str) :
       error = result.stderr
        
       #Sending the file path to the fixer
-      fix_llm = get_fix(test_file , error)
+      fix_llm = get_fix(function , error)
 
       #Printing llm proposed fix
       print("Proposed Fix : " , fix_llm)
 
        #Writing the fix to the file path
-      with open(test_file, "w") as f:
-        f.write(fix_llm)
+      with open(fixed_file, "w") as f:
+        f.write(fix_llm + "\n" + assert_statements)
 
    
 
