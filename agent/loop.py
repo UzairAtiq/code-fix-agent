939dd8b016ec8bdcec6df2a23540c882231e5856
UzairAtiq <uzairatiq65@gmail.com>
2026-09-08 15:51:02 +0500

    feat: add attempt count and output print statements to fix_loop

--- agent/loop.py
+++ agent/loop.py
@@ -13,19 +13,36 @@ def fix_loop (test_file : str) :
     #Incrementing count
     count += 1
 
+    #Printing the number of times the loop ran 
+    print("Attempt # : ",count)
+
     #Running the file through the runner(executor)
     result = runner(test_file)
 
     #If no error
     if result.returncode == 0 or stopper(count):
+      print("FILE RAN SUCESSFULLY")
+
+      #Printing the output 
+      print("File Output : ",result.stdout)
       break
+
     else : 
-        #Sending the file path to the fixer
-       fix_llm = get_fix(test_file)
+
+      #Getting the error
+      error = result.stderr
+       
+      #Sending the file path to the fixer
+      fix_llm = get_fix(test_file , error)
+
+      #Printing llm proposed fix
+      print("Proposed Fix : " , fix_llm)
 
        #Writing the fix to the file path
-       with open(test_file, "w") as f:
+      with open(test_file, "w") as f:
         f.write(fix_llm)
+
+   
 
 
 
