abbba04b6270d63a07a4b5025a66bbf50f0f818a
UzairAtiq <uzairatiq65@gmail.com>
2026-09-08 15:15:34 +0500

    chore: no functional changes to runner.py

--- sandbox/runner.py
+++ sandbox/runner.py
@@ -5,7 +5,7 @@ def runner(test_file : str) :
   #creating the subprocess 
   result = subprocess.run(  ["python" ,test_file] , capture_output=True , text= True)
 
-
   #returning the subprocess result 
   return result
+
 
