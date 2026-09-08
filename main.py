d88789c1b10e1a0b15640648ab9357f87c7e4e38
UzairAtiq <uzairatiq65@gmail.com>
2026-09-08 16:40:32 +0500

    test: pass hardcoded fixed_file path to fix_loop for testing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from agent.loop import fix_loop
 
 test_file = '/Users/uzair/Developer/code-fix-agent/tests/sample_broken/test1.py'
+fixed_file = "/Users/uzair/Developer/code-fix-agent/tests/fixed/test1.py"
 
-fix_loop(test_file)
+fix_loop(test_file , fixed_file)
